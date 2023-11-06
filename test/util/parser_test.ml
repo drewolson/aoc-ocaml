e3a4c5aaed0d2b31bf5a19aacb74ac69b2d2a352
drew <drew@drewolson.org>
2023-11-05 19:40:42 -0600

    Parse all input for parser tests

--- test/util/parser_test.ml
+++ test/util/parser_test.ml
@@ -1,19 +1,19 @@
 module P = Util.Parser
 
 let%expect_test "signed_integer works with positive signs" =
-  let result = P.parse_exn P.signed_integer "+10" in
+  let result = P.parse_all_exn P.signed_integer "+10" in
   Printf.printf "%i" result;
   [%expect {| 10 |}]
 ;;
 
 let%expect_test "signed_integer works with negative signs" =
-  let result = P.parse_exn P.signed_integer "-10" in
+  let result = P.parse_all_exn P.signed_integer "-10" in
   Printf.printf "%i" result;
   [%expect {| -10 |}]
 ;;
 
 let%expect_test "signed_integer works with no signs" =
-  let result = P.parse_exn P.signed_integer "10" in
+  let result = P.parse_all_exn P.signed_integer "10" in
   Printf.printf "%i" result;
   [%expect {| 10 |}]
 ;;
