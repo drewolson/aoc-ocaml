ea63026bdf5c16ccd02b1b491509a253c17577b9
drew <drew@drewolson.org>
2023-12-01 08:49:03 -0600

    Update

--- lib/year2023/day01.ml
+++ lib/year2023/day01.ml
@@ -2,7 +2,7 @@ module StrMap = Map.Make (String)
 
 let words = [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
 let digits = List.range 1 10 |> List.map ~f:Int.to_string
-let mapping = digits |> List.zip_exn words |> StrMap.of_alist_exn
+let mapping = List.zip_exn words digits |> StrMap.of_alist_exn
 
 let re_f =
   words |> List.append digits |> String.concat ~sep:"|" |> Re.Pcre.re |> Re.compile
