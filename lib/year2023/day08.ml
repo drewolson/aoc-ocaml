a64b319d953caea22cd420abd3b464cf957a4718
drew <drew@drewolson.org>
2023-12-08 18:57:42 +0800

    Day 8

---
+++ lib/year2023/day08.ml
@@ -0,0 +1,88 @@
+module StrMap = Map.Make (String)
+module P = Util.Parser
+open P.Syntax
+
+type dir =
+  | R
+  | L
+
+type node = string * string
+
+type input =
+  { dirs : dir Sequence.t
+  ; map : node StrMap.t
+  }
+
+let dir_p = P.choice [ R <$ P.char 'R'; L <$ P.char 'L' ]
+let dirs_p = P.many_till dir_p P.end_of_line >>| Sequence.cycle_list_exn
+
+let token_p =
+  P.take_while (function
+    | 'A' .. 'Z' -> true
+    | '0' .. '9' -> true
+    | _ -> false)
+;;
+
+let pair_p =
+  let%map left = P.char '(' *> token_p <* P.string ", "
+  and right = token_p <* P.char ')' in
+  left, right
+;;
+
+let entry_p =
+  let%map key = token_p <* P.string " = "
+  and value = pair_p in
+  key, value
+;;
+
+let map_p = P.sep_by1 P.end_of_line entry_p >>| StrMap.of_alist_exn
+
+let input_p =
+  let%map dirs = dirs_p <* P.end_of_line
+  and map = map_p in
+  { dirs; map }
+;;
+
+let solve { dirs; map } =
+  let make_move (count, node) dir =
+    if String.equal node "ZZZ"
+    then Error count
+    else (
+      let l, r = Map.find_exn map node in
+      match dir with
+      | L -> Ok (count + 1, l)
+      | R -> Ok (count + 1, r))
+  in
+  dirs
+  |> Sequence.fold_result ~init:(0, "AAA") ~f:make_move
+  |> Result.error
+  |> Option.value_exn
+;;
+
+let solve' { dirs; map } =
+  let is_start s = s |> String.to_list_rev |> List.hd_exn |> Char.equal 'A' in
+  let is_end s = s |> String.to_list_rev |> List.hd_exn |> Char.equal 'Z' in
+  let starts = map |> Map.keys |> List.filter ~f:is_start in
+  let make_move (count, steps, nodes) dir =
+    let ends, rest = List.partition_tf nodes ~f:is_end in
+    let steps' = if List.is_empty ends then steps else count :: steps in
+    let nodes' =
+      List.map rest ~f:(fun node ->
+        let l, r = Map.find_exn map node in
+        match dir with
+        | L -> l
+        | R -> r)
+    in
+    if List.is_empty nodes' then Error steps' else Ok (count + 1, steps', nodes')
+  in
+  dirs
+  |> Sequence.fold_result ~init:(0, [], starts) ~f:make_move
+  |> Result.error
+  |> Option.value_exn
+  |> List.map ~f:Z.of_int
+  |> List.fold ~init:Z.one ~f:Z.lcm
+  |> Z.to_string
+;;
+
+let part1 input = input |> P.parse_exn input_p |> solve
+let part2 input = input |> P.parse_exn input_p |> solve'
---
+++ test/year2023/day08_test.ml
@@ -0,0 +1,45 @@
+let input1 =
+  {|RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+|}
+;;
+
+let input2 = {|LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+|}
+
+let input3 =
+  {|LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+|}
+;;
+
+let%expect_test "2023 day8 part1" =
+  Printf.printf "%i" @@ Year2023.Day08.part1 input1;
+  [%expect {| 2 |}];
+  Printf.printf "%i" @@ Year2023.Day08.part1 input2;
+  [%expect {| 6 |}]
+;;
+
+let%expect_test "2023 day8 part2" =
+  Printf.printf "%s" @@ Year2023.Day08.part2 input3;
+  [%expect {| 6 |}]
+;;
