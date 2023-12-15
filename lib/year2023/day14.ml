d974f411fb9afbeb1329e2c96ea949040d309c2c
drew <drew@drewolson.org>
2023-12-14 20:53:42 -0600

    Naming

--- lib/year2023/day14.ml
+++ lib/year2023/day14.ml
@@ -5,15 +5,18 @@ end
 let rotate grid = grid |> List.transpose_exn |> List.map ~f:List.rev
 let parse input = input |> String.split_lines |> List.map ~f:String.to_list |> rotate
 
-let shift_rocks l =
+let tilt grid =
   let aux (rocks, l) = function
     | 'O' -> 'O' :: rocks, l
     | '.' -> rocks, '.' :: l
     | '#' -> [], ('#' :: rocks) @ l
     | _ -> rocks, l
   in
-  let rocks, l' = List.fold l ~init:([], []) ~f:aux in
-  rocks @ l' |> List.rev
+  let shift_rocks l =
+    let rocks, l' = List.fold l ~init:([], []) ~f:aux in
+    rocks @ l' |> List.rev
+  in
+  List.map grid ~f:shift_rocks
 ;;
 
 let load grid =
@@ -22,13 +25,11 @@ let load grid =
   |> Util.List.sum_int
 ;;
 
-let cycle grid =
-  List.fold (List.range 0 4) ~init:grid ~f:(fun acc _ ->
-    acc |> List.map ~f:shift_rocks |> rotate)
-;;
-
-let run_cycles n grid =
+let run_cycles n =
   let cache = Hashtbl.create (module Key) in
+  let cycle grid =
+    List.fold (List.range 0 4) ~init:grid ~f:(fun acc _ -> acc |> tilt |> rotate)
+  in
   let rec aux n grid =
     match n with
     | 0 -> grid
@@ -39,11 +40,8 @@ let run_cycles n grid =
          Hashtbl.add_exn cache ~key:grid ~data:n;
          grid |> cycle |> aux (n - 1))
   in
-  aux n grid
+  aux n
 ;;
 
-let part1 input =
-  input |> parse |> List.map ~f:shift_rocks |> List.sum (module Int) ~f:load
-;;
-
+let part1 input = input |> parse |> tilt |> List.sum (module Int) ~f:load
 let part2 input = input |> parse |> run_cycles 1000000000 |> List.sum (module Int) ~f:load
